5b38ab43b9fc3ed431370b73b255011844fd6189
j-stax <ahnjames01@gmail.com>
2025-04-13 09:39:36 -0400

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header'
+import './App.scss'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
