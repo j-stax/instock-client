42ee1052350f5a64c99dc5db242e76743cf22122
j-stax <ahnjames01@gmail.com>
2025-04-14 19:10:28 -0400

    Header.jsx - tablet and desktop styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,6 +21,8 @@ export default function Header() {
         }
     })
 
+    // TODO: Tablet and Desktop view
+
     return (
         <header className="header">
             <Logo className="header__logo" />
