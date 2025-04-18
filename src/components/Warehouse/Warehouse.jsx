cc731f8807e7f3e66cd21d9e927f8ea13e8b8930
j-stax <ahnjames01@gmail.com>
2025-04-18 18:43:11 -0400

    Warehouse component - tablet view

--- src/components/Warehouse/Warehouse.jsx
+++ src/components/Warehouse/Warehouse.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 export default function Warehouse({ name, address, contactName, contactInfo }) {
     return (
         <div className="wh-component">
-            <div className="wh-component__sections-container">
+            <div className="wh-component__sections-container wh-component__sections-container--mobile">
                 <div className="wh-component__left">
                     <div className="wh-component__section">
                         <p className="wh-component__label">WAREHOUSE</p>
@@ -36,9 +36,38 @@ export default function Warehouse({ name, address, contactName, contactInfo }) {
                     </div>
                 </div>
             </div>
-            <div className="wh-component__footer">
+            <div className="wh-component__footer wh-component__footer--mobile">
                 <DelIcon className="wh-component__icon wh-component__icon--trash" />
                 <EditIcon className="wh-component__icon wh-component__icon--edit" />
+            </div>
+            <div className="wh-component__sections-container wh-component__sections-container--tablet">
+                <div className="wh-component__section">
+                    <p className="wh-component__label">WAREHOUSE</p>
+                    <h3 className="wh-component__content">
+                        <Link to="#" className="wh-component__name">
+                            {name}
+                            <ChevronIcon className="wh-component__chevron" />
+                        </Link>
+                    </h3>
+                </div>
+                <div className="wh-component__section">
+                    <p className="wh-component__label">ADDRESS</p>
+                    <p className="wh-component__content">{address.street}</p>
+                    <p className="wh-component__content">{`${address.city}, ${address.country}`}</p>
+                </div>
+                <div className="wh-component__section">
+                    <p className="wh-component__label">CONTACT NAME</p>
+                    <p className="wh-component__content">{contactName}</p>
+                </div>
+                <div className="wh-component__section">
+                    <p className="wh-component__label">CONTACT INFORMATION</p>
+                    <p className="wh-component__content">{contactInfo.phone}</p>
+                    <p className="wh-component__content">{contactInfo.email}</p>
+                </div>
+                <div className="wh-component__footer wh-component__footer--tablet">
+                    <DelIcon className="wh-component__icon wh-component__icon--trash" />
+                    <EditIcon className="wh-component__icon wh-component__icon--edit" />
+                </div>
             </div>
         </div>
     )
