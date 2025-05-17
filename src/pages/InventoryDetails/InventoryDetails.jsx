2033c58a36d7190b20ce855d58a3e591d6e8d575
j-stax <ahnjames01@gmail.com>
2025-05-16 23:09:59 -0400

    InventoryDetails - tablet view

--- src/pages/InventoryDetails/InventoryDetails.jsx
+++ src/pages/InventoryDetails/InventoryDetails.jsx
@@ -29,34 +29,41 @@ export default function InventoryDetails() {
                         <Link className="inv-details__edit-icon-link">
                             <div className="inv-details__edit-icon-background">
                                 <EditIcon className="inv-details__edit-icon" />
+                                <span className="inv-details__edit-text">Edit</span>
                             </div>
                         </Link>
                     </div>
                 </div>
                 <div className="inv-details__item">
-                    <div className="inv-details__content-section">
-                        <h4 className="inv-details__item-label">ITEM DESCRIPTION</h4>
-                        <p className="inv-details__item-text">This 50", 4K LED TV provides a crystal-clear picture and vivid colors.</p>
-                    </div>
-                    <div className="inv-details__content-section">
-                        <h4 className="inv-details__item-label">CATEGORY</h4>
-                        <p className="inv-details__item-text">Electronics</p>
-                    </div>
-                    <div className="inv-details__status-quantity-row">
-                        <div>
-                            <h4 className="inv-details__item-label">STATUS</h4>
-                            <p 
-                                className="inv-details__item-text inv-details__item-text--status"
-                            >IN STOCK</p>
+                    <div className="inv-details__tablet-section-left">
+                        <div className="inv-details__content-section">
+                            <h4 className="inv-details__item-label">ITEM DESCRIPTION</h4>
+                            <p className="inv-details__item-text inv-details__item-text--description">
+                                This 50", 4K LED TV provides a crystal-clear picture and vivid colors.
+                            </p>
                         </div>
-                        <div>
+                        <div className="inv-details__content-section">
                             <h4 className="inv-details__item-label">CATEGORY</h4>
                             <p className="inv-details__item-text">Electronics</p>
                         </div>
                     </div>
-                    <div className="inv-details__content-section">
-                        <h4 className="inv-details__item-label">WAREHOUSE</h4>
-                        <p className="inv-details__item-text">Manhattan</p>
+                    <div className="inv-details__tablet-section-right">
+                        <div className="inv-details__status-quantity-row">
+                            <div>
+                                <h4 className="inv-details__item-label">STATUS</h4>
+                                <p 
+                                    className="inv-details__item-text inv-details__item-text--status"
+                                >IN STOCK</p>
+                            </div>
+                            <div>
+                                <h4 className="inv-details__item-label">QUANTITY</h4>
+                                <p className="inv-details__item-text">500</p>
+                            </div>
+                        </div>
+                        <div className="inv-details__content-section">
+                            <h4 className="inv-details__item-label">WAREHOUSE</h4>
+                            <p className="inv-details__item-text">Manhattan</p>
+                        </div>
                     </div>
                 </div>     
             </div>
